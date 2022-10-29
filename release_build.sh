a6255ae04a2b3aa277ef9709741e79a539fdc2f8
Mustafa ÖNCEL <mustafaoncel92@gmail.com>
2022-10-30 00:52:32 +0300

    Only use base x86_64 instructions on CI binary
    Use base x86_64 instructions but only when --portable is passed, which is the case on GitHub Actions.

--- release_build.sh
+++ release_build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 ./update_rust.sh
 ./test.sh
-RUSTFLAGS="--remap-path-prefix=/home/$USER/.cargo/=/.cargo/ -C opt-level=3 -C target-cpu=native -Z tune-cpu=native -C lto -Z mir-opt-level=4" cargo build -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort --target=x86_64-unknown-linux-gnu --release
+if [ "$1" == "--portable" ]; then
+ RUSTFLAGS="--remap-path-prefix=/home/$USER/.cargo/=/.cargo/ -C opt-level=3 -C target-cpu=x86-64 -Z tune-cpu=x86-64 -C lto -Z mir-opt-level=4" cargo build -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort --target=x86_64-unknown-linux-gnu --release
+else
+ RUSTFLAGS="--remap-path-prefix=/home/$USER/.cargo/=/.cargo/ -C opt-level=3 -C target-cpu=native -Z tune-cpu=native -C lto -Z mir-opt-level=4" cargo build -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort --target=x86_64-unknown-linux-gnu --release
+fi
 strip target/x86_64-unknown-linux-gnu/release/hypixel-skyblock-util-tools
 #upx --best target/x86_64-unknown-linux-gnu/release/hypixel-skyblock-util-tools
 
