65a5a3819ca7cbc88bf0db417a5dc13e4b401864
Mustafa ÖNCEL <mustafaoncel92@gmail.com>
2022-10-31 02:08:23 +0300

    fix mtune value

--- release_build.sh
+++ release_build.sh
@@ -2,7 +2,7 @@
 ./update_rust.sh
 ./test.sh
 if [ "$1" == "--portable" ]; then
- RUSTFLAGS="--remap-path-prefix=/home/$USER/.cargo/=/.cargo/ -C opt-level=3 -C target-cpu=x86-64 -Z tune-cpu=x86-64 -C lto -Z mir-opt-level=4" cargo build -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort --target=x86_64-unknown-linux-gnu --release
+ RUSTFLAGS="--remap-path-prefix=/home/$USER/.cargo/=/.cargo/ -C opt-level=3 -C target-cpu=x86-64 -Z tune-cpu=generic -C lto -Z mir-opt-level=4" cargo build -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort --target=x86_64-unknown-linux-gnu --release
 else
  RUSTFLAGS="--remap-path-prefix=/home/$USER/.cargo/=/.cargo/ -C opt-level=3 -C target-cpu=native -Z tune-cpu=native -C lto -Z mir-opt-level=4" cargo build -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort --target=x86_64-unknown-linux-gnu --release
 fi
