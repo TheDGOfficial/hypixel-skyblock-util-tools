a427f1270f67ad9aa04bfa792605d563cbe50232
Mustafa ÖNCEL <mustafaoncel92@gmail.com>
2022-11-15 18:27:18 +0300

    Make run script work without mold installed
    Mold is still recommended and used when available.

--- run.sh
+++ run.sh
@@ -13,10 +13,18 @@ fi
 # Plus theres a problem where cargo recompiles all crates everytime ignoring target/ even if its contents can be used to cache, no idea how to fix. This doesn't happen if we disable ramdisk. This makes it painfully slow because cargo clippy re-compiles everything, AND then cargo run recompiles everything again..
 #./ramdisk_create.sh
 
+MOLD_WRAPPER_CMD="mold -run"
+
+if ! command -v mold &> /dev/null
+then
+    echo "Mold is not available, using (slower) default linker"
+    MOLD_WRAPPER_CMD=""
+fi
+
 if [ "$1" == "--skip-extra-analyzers" ] || [ "$2" == "--skip-extra-analyzers" ]; then
- mold -run cargo run
+ $MOLD_WRAPPER_CMD cargo run
 else
- mold -run cargo clippy -- -W clippy::all -W clippy::style -W clippy::pedantic -W clippy::nursery -W clippy::perf -W clippy::suspicious -W clippy::cargo -W clippy::restriction -W clippy::exit -W clippy::dbg_macro -W clippy::unwrap_used -W clippy::complexity -W clippy::create_dir -W clippy::correctness -W clippy::expect_used -W clippy::too-many-lines -W clippy::must-use-candidate -W clippy::multiple-crate-versions -A clippy::print-stdout -A clippy::use-debug -A clippy::missing-docs-in-private-items -A clippy::implicit-return -A clippy::default-numeric-fallback -A clippy::float-arithmetic -A clippy::integer-arithmetic -A clippy::arithmetic-side-effects -A clippy::integer-division -A clippy::get-unwrap -A clippy::redundant-pub-crate && mold -run cargo run
+ $MOLD_WRAPPER_CMD cargo clippy -- -W clippy::all -W clippy::style -W clippy::pedantic -W clippy::nursery -W clippy::perf -W clippy::suspicious -W clippy::cargo -W clippy::restriction -W clippy::exit -W clippy::dbg_macro -W clippy::unwrap_used -W clippy::complexity -W clippy::create_dir -W clippy::correctness -W clippy::expect_used -W clippy::too-many-lines -W clippy::must-use-candidate -W clippy::multiple-crate-versions -A clippy::print-stdout -A clippy::use-debug -A clippy::missing-docs-in-private-items -A clippy::implicit-return -A clippy::default-numeric-fallback -A clippy::float-arithmetic -A clippy::integer-arithmetic -A clippy::arithmetic-side-effects -A clippy::integer-division -A clippy::get-unwrap -A clippy::redundant-pub-crate && $MOLD_WRAPPER_CMD cargo run
  #MIRAI_FLAGS=--diag=paranoid cargo mirai
 fi
 
