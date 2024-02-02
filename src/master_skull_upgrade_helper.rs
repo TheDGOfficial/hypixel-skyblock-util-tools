254d545ca2e1e093d3264dd4092689a2f315231a
Mustafa ÖNCEL <mustafaoncel92@gmail.com>
2024-02-02 03:41:05 +0300

    set minimum tls version to 1.3

--- src/master_skull_upgrade_helper.rs
+++ src/master_skull_upgrade_helper.rs
@@ -7,6 +7,7 @@ use futures::stream::FuturesOrdered;
 use futures::StreamExt;
 use nohash_hasher::BuildNoHashHasher;
 use nohash_hasher::IntMap;
+use reqwest::tls::Version;
 use reqwest::Error;
 use reqwest::Response;
 use serde_json::Value;
@@ -147,6 +148,7 @@ async fn do_requests_and_extract_prices(
     let mut requests = Vec::with_capacity(7);
     let client = reqwest::ClientBuilder::new()
         .timeout(Duration::from_secs(10))
+        .min_tls_version(Version::TLS_1_3)
         .brotli(true)
         .build();
 
