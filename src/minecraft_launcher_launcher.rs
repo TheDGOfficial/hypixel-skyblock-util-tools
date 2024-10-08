0acf34b6e5fc0f98d613527ef78b714f4a21a893
Mustafa ÖNCEL <mustafaoncel92@gmail.com>
2024-10-08 19:01:07 +0300

    fix compilation errors with new sysinfo

--- src/minecraft_launcher_launcher.rs
+++ src/minecraft_launcher_launcher.rs
@@ -244,6 +244,7 @@ fn find_launcher_processes(
 
     sys.refresh_processes_specifics(
         ProcessesToUpdate::All,
+        true,
         ProcessRefreshKind::new()
             .with_cmd(UpdateKind::OnlyIfNotSet)
             .with_user(UpdateKind::OnlyIfNotSet),
@@ -359,6 +360,7 @@ fn start_watching_java_process() {
 
                                 if sys.refresh_processes_specifics(
                                     ProcessesToUpdate::Some(&[pid]),
+                                    true,
                                     ProcessRefreshKind::new()
                                         .with_cmd(UpdateKind::OnlyIfNotSet),
                                 ) == 1 {
